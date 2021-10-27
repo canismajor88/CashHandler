a27953187257fbfb42a3a9432f9e165549a51fea
canismajor88 <abstfact87@gmail.com>
2021-10-26 20:47:53 -0500

    just about emails working

--- CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/Models/UserCredential.cs
+++ CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/Models/UserCredential.cs
@@ -10,8 +10,10 @@ namespace CashHandlerAPI.Models
     {
         #region public properties
         //init ( initialize instead of set because records are immutable)records have build in comparison operators so we don't have to make our own
-        public string UserName { get; init; }
-        public string Password { get; init; }
+        public string? UserName { get; init; }
+        public string? Password { get; init; }
+
+        public string? Email { get; init; }
         #endregion
     }
 }
