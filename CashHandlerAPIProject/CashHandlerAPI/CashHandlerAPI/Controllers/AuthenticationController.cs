a3c10348d90b36ed2d7fcfce61dbb3df0905ee17
canismajor88 <abstfact87@gmail.com>
2021-10-27 14:14:51 -0500

    just about working need to setup data base

--- CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/Controllers/AuthenticationController.cs
+++ CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/Controllers/AuthenticationController.cs
@@ -10,6 +10,7 @@ using CashHandlerAPI.Helper;
 using CashHandlerAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -26,17 +27,20 @@ namespace CashHandlerAPI.Controllers
         private readonly ITokenGenerator _tokenGenerator;
         private readonly IEmailHelper _emailHelper;
         private readonly IOptions<EmailOptions> _emailOptions;
+        private readonly UserManager<User> _userManager;
+
         #endregion
 
         #region constructors
         public AuthenticationController(ILogger<AuthenticationController> logger, IUserCredentialsRepo userRepo, ITokenGenerator tokenGenerator
-            , IEmailHelper emailHelper, IOptions<EmailOptions> emailOptions)
+            , IEmailHelper emailHelper, IOptions<EmailOptions> emailOptions ,UserManager<User> userManager)
         {
             _logger = logger;
             _iUserCredentialsRepo = userRepo;
             _tokenGenerator = tokenGenerator;
             _emailHelper = emailHelper;
             _emailOptions = emailOptions;
+            _userManager = userManager;
         }
         #endregion
 
@@ -79,12 +83,18 @@ namespace CashHandlerAPI.Controllers
         [HttpPost]
         [Consumes(MediaTypeNames.Application.Json)]
         [Route("register")]
-        public async Task<IActionResult> Register([FromBody] UserCredential userCredential)
+        public async Task<IActionResult> Register([FromBody] CreateUserCredential userCredential)
         {
             try
             {
                 var isFound = await _iUserCredentialsRepo.IsUser(userCredential);
-                if (!isFound)
+                var newUser = new User
+                {
+                    UserName = userCredential.UserName,
+                    Email = userCredential.Email
+                };
+                var result = await _userManager.CreateAsync(newUser, userCredential.Password);
+                if (!isFound&&result.Succeeded)
                 { _logger.Log(LogLevel.Information,"user was added");
                     if(await _iUserCredentialsRepo.AddUser(userCredential))
                         //json sending back
@@ -95,10 +105,7 @@ namespace CashHandlerAPI.Controllers
                         });
                 }
                 _logger.Log(LogLevel.Information, "user was not added");
-                return StatusCode(StatusCodes.Status401Unauthorized, new
-                {
-                    succeeded = false
-                });
+                return BadRequest(result);
             }
             catch
             {
---
+++ CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashHandlerAPI.Data
+{
+    public class ApplicationDBContext:IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options){}
+
+    }
+}
