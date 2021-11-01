e0761d4a51df69af1025dfbb282ecf8c4473dff1
canismajor88 <abstfact87@gmail.com>
2021-11-01 16:21:48 -0500

    got user creation and moneyAmounts to work

--- CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/ViewModels/EmailOptions.cs
+++ CashHandlerAPIProject/CashHandlerAPI/CashHandlerAPI/ViewModels/EmailOptions.cs
@@ -3,9 +3,10 @@
     public record EmailOptions
     {
         public string Host { get; set; }
-        public string UserName_SenderEmail { get; set; }
+        public string ApiKey { get; set; }
         public string ApiKeySecret { get; set; }
         public int Port { get; set; }
+        public string SenderEmail { get; set; }
 
     }
 }
