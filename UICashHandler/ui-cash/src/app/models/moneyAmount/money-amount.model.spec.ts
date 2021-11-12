5a6041e29e08d2e000a85bf53a29d8986baf4fad
canismajor88 <abstfact87@gmail.com>
2021-11-11 21:55:04 -0600

    Folder re struct (#29)
    * Reformatted File Structure
    
    * fixed stuff

--- UICashHandler/ui-cash/src/app/models/moneyAmount/money-amount.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MoneyAmount } from './money-amount.model';
-
-describe('MoneyAmount', () => {
-  it('should create an instance', () => {
-    expect(new MoneyAmount()).toBeTruthy();
-  });
-});
